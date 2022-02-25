82d1accb4f9e0d32785c38fec3fed87a7d88961b
sergiologino <savkin.sergey@yandex.ru>
2022-02-26 01:16:00 +0300

    first commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,25 +3,29 @@ public class Calculator {
 
     public static void main(String args[]) {
       Scanner scan = new Scanner(System.in);
+
       System.out.print("Введите операнд1 ");
       int operand =  scan.nextInt();
       int result = 0;
       System.out.print("Введите операцию ");
       char operator = scan.next().charAt(0);
-      System.out.print("Введите операнд2 ");
       switch (operator) {
          case '*':
-            result = operand * scan.nextInt();
-            break;
+             System.out.print("Введите операнд2 ");
+             result = operand * scan.nextInt();
+             break;
          case '/':
-            result = operand / scan.nextInt();
-            break;
+             System.out.print("Введите операнд2 ");
+             result = operand / scan.nextInt();
+             break;
          case '+':
-            result = operand + scan.nextInt();
-            break;
+             System.out.print("Введите операнд2 ");
+             result = operand + scan.nextInt();
+             break;
          case '-':
-            result = operand - scan.nextInt();
-            break;
+             System.out.print("Введите операнд2 ");
+             result = operand - scan.nextInt();
+             break;
          default:
             System.out.println("Неправильный оператор");
       }
